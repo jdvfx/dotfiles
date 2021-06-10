98646e8791ec5c9a11acad536e1111549904abc4
Julien Depredurand <julien.depredurand.com>
2021-06-09 22:41:06 -0700

    updated FONT names for xmobar/xmonad

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -42,5 +42,5 @@ myConfig = def {
                , ((mod4Mask, xK_F8), spawn "/bin/python2.7 ~/.xmonad/brightness/adjustBrightness.py +.1")
                , ((mod4Mask, xK_Right), spawn "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Next")
                , ((mod4Mask, xK_Left), spawn "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Previous")
-               , ((mod4Mask, xK_o), spawn "ls --color=never --format=single-column ~/.local/bin/dmenu/ | dmenu -fn 'JetBrains Mono NL:size=9' -sb '#004400' -nb '#000000' | xargs -I % sh -c '~/.local/bin/dmenu/%'")
+               , ((mod4Mask, xK_o), spawn "ls --color=never --format=single-column ~/.local/bin/dmenu/ | dmenu -fn 'JetBrains Mono:size=9' -sb '#004400' -nb '#000000' | xargs -I % sh -c '~/.local/bin/dmenu/%'")
                ]
