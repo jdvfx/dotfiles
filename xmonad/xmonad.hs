3ddf1999fcceb153b8e4af3389ffdbe46dbb6035
Julien Depredurand <julien.depredurand@gmail.com>
2021-06-10 22:52:44 -0700

    xmobar colors update, statup hook added to fix wallpaper and keyboard remaps (SL del, VIM capslock>Esc)

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -3,29 +3,54 @@
 import XMonad
 import XMonad.Util.EZConfig
 import XMonad.Hooks.DynamicLog
+import XMonad.Util.SpawnOnce
+
 
 -- The main function.
 main = xmonad =<< statusBar myBar myPP toggleStrutsKey myConfig
-
+-- statusBar 
 -- Command to launch the bar.
 myBar = "xmobar"
 
 -- Custom PP, configure it as you like. It determines what is being written to the bar.
-myPP = xmobarPP { ppCurrent = xmobarColor "#429942" "" . wrap "<" ">" }
+myPP = xmobarPP { 
+                ppCurrent = xmobarColor "#1ac3ff" "",
+                ppTitle = xmobarColor "#444444" ""
+                }
+
+--myPP = xmobarPP { ppCurrent = xmobarColor "#429942" "" . wrap "<" ">" }
+
+                      --"" . wrap "<" ">"
+
+--xmobarPP {
+            --ppOutput = hPutStrLn xmproc
+          --, ppTitle = xmobarColor xmobarTitleColor "" . shorten 100
+          --, ppCurrent = xmobarColor xmobarCurrentWorkspaceColor ""
+          --, ppSep = "   "
+      --} 
 
 -- Key binding to toggle the gap for the bar.
 -- B key: show/hide the bar
 toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
 
 
-
+------------------------------------------------------------------------
+-- AUTOSTART
+------------------------------------------------------------------------
+myStartupHook :: X ()
+myStartupHook = do
+    spawnOnce "nitrogen --restore &" 
+    spawnOnce "tput smkx"
+    spawnOnce "setxkbmap -option 'caps:escape'"
+               
 -- Main configuration, override the defaults to your liking.
 myConfig = def {
                modMask = mod4Mask, -- Use Super instead of Alt
                terminal = "st",
                borderWidth   = 1,
                normalBorderColor  = "#000000",
-               focusedBorderColor = "#0088FF"
+               focusedBorderColor = "#0088FF",
+               startupHook        = myStartupHook
                } `additionalKeys`
                [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock; xset dpms force off")
                , ((mod4Mask, xK_Print), spawn "scrot ~/Pictures/screenshots/")
@@ -42,5 +67,5 @@ myConfig = def {
                , ((mod4Mask, xK_F8), spawn "/bin/python2.7 ~/.xmonad/brightness/adjustBrightness.py +.1")
                , ((mod4Mask, xK_Right), spawn "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Next")
                , ((mod4Mask, xK_Left), spawn "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Previous")
-               , ((mod4Mask, xK_o), spawn "ls --color=never --format=single-column ~/.local/bin/dmenu/ | dmenu -fn 'JetBrains Mono:size=9' -sb '#004400' -nb '#000000' | xargs -I % sh -c '~/.local/bin/dmenu/%'")
+               , ((mod4Mask, xK_o), spawn "ls --color=never --format=single-column ~/.local/bin/dmenu/ | dmenu -fn 'JetBrains Mono NL:size=9' -sb '#004400' -nb '#000000' | xargs -I % sh -c '~/.local/bin/dmenu/%'")
                ]
