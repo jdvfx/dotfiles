4458ea71fea8062ca0361f30854da196a82b6926
Julien Depredurand <julien.depredurand@gmail.com>
2022-05-18 21:10:20 -0700

    reverted config

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -41,13 +41,9 @@ myLayouts = layoutTall ||| layoutSpiral ||| layoutGrid ||| layoutMirror ||| noBo
 ------------------------------------------------------------------------
 -- AUTOSTART
 ------------------------------------------------------------------------
--- WINIT_X11_SCALE_FACTOR is used for alacritty
--- it displays the same font size on monitors
--- with different DPIs
 --
 myStartupHook :: X ()
 myStartupHook = do
-    spawnOnce "export WINIT_X11_SCALE_FACTOR=1"
     spawnOnce "nitrogen --restore &"
     spawnOnce "compton  &"
 
