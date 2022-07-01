422dc034e19c4f1cfc69c8b7a1fc00edde4e7494
Julien Depredurand <julien.depredurand@gmail.com>
2022-06-30 22:17:46 -0700

    updated tokyodark conf

---
+++ nvim/.config/nvim/lua/config/tokyodark.lua
@@ -0,0 +1,6 @@
+-- init.lua
+vim.g.tokyodark_transparent_background = true
+vim.g.tokyodark_enable_italic_comment = true
+vim.g.tokyodark_enable_italic = false
+vim.g.tokyodark_color_gamma = "1"
+vim.cmd("colorscheme tokyodark")
