d2938d038ecf0a5a42f8b9a5098b38815dabcc04
Julien Depredurand <julien.depredurand@gmail.com>
2022-09-06 21:55:27 -0700

    leader+p find files in the current project

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -212,6 +212,8 @@ vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
+-- BUNKER - leader+p to find files in the current project
+vim.keymap.set('n', '<leader>p', require('telescope.builtin').find_files,{})
 
 -- [[ Configure Treesitter ]]
 -- See `:help nvim-treesitter`
