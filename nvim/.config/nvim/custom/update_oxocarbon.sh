9bb21eda46bd600e72e2ec06c3665e3181a9ed90
Julien Depredurand <julien.depredurand@gmail.com>
2023-03-22 23:30:54 -0700

    color patch for oxocarbon

---
+++ nvim/.config/nvim/custom/update_oxocarbon.sh
@@ -0,0 +1,2 @@
+cp ~/.local/share/nvim/lazy/oxocarbon.nvim/lua/oxocarbon/init.lua ~/.local/share/nvim/lazy/oxocarbon.nvim/lua/oxocarbon/init.lua_bak
+cp oxo_colors.lua ~/.local/share/nvim/lazy/oxocarbon.nvim/lua/oxocarbon/init.lua_bak
