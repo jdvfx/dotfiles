bb23788a5c093b61beecf043a3572f0bb15b3fe6
Julien Depredurand <julien.depredurand@gmail.com>
2023-09-03 14:40:38 -0700

    colors update

--- nvim/.config/nvim/custom/update_tokyodark.sh
+++
@@ -1,5 +0,0 @@
-cd ~/.local/share/nvim/site/pack/packer/start/tokyodark.nvim/lua/tokyodark/
-cp palette.lua palette.lua_bak
-cp highlights.lua highlights.lua_bak
-cd -
-cp *.lua ~/.local/share/nvim/site/pack/packer/start/tokyodark.nvim/lua/tokyodark/
