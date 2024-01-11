ed2c5203e558cb360b70968b571fa6a696cef326
jdvfx <ju.dep@zoho.com>
2024-01-10 22:30:34 -0800

    cleanup status, added cpu load

--- sway/.config/sway/status.sh
+++ sway/.config/sway/status.sh
@@ -1,46 +1,34 @@
 #!/bin/bash
-#
-# The Sway configuration file in ~/.config/sway/config calls this script.
-# You should see changes to the status bar after saving this script.
-# If not, do "killall swaybar" and $mod+Shift+c to reload the configuration.
+date_formatted=$(date "+%a.%d.%b(%m).%Y %H:%M")
 
-# Produces "21 days", for example
-# uptime_formatted=$(uptime | cut -d ',' -f1  | cut -d ' ' -f4,5)
-# upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep percentage | awk '{print $2}'
-# The abbreviated weekday (e.g., "Sat"), followed by the ISO-formatted date
-# like 2018-10-06 and the time (e.g., 14:01)
-date_formatted=$(date "+%a,%d,%m(%b),%Y . %H:%M")
+# only care about that for Thinkpad
+# battery=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep percentage | awk '{print $2}')
 
-# Get the Linux version but remove the "-1-ARCH" part
-# linux_version=$(uname -r | cut -d '-' -f1)
-
-battery=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep percentage | awk '{print $2}')
+# ---------- CPU average usage ---------
+cpu=$(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1"%"}')
 
 # ---------- CPU temp ---------
-# # lenovo thinkpad
+# Thinkpad
 # cpu_temp=$(sensors| grep CPU | cut -d + -f2)
 
-# macbook pro
+# Macbook pro
 cpu_temp=$(sensors | grep "Core 0" | awk '{print $3}')
 
 
 # ---------- FANS ---------
 
-# lenovo thinkpad
+# Thinkpad
 # fan1=$(cat /proc/acpi/ibm/fan | grep level: | awk '{print $2}')
 # fan2=$(sensors| grep fan2 | awk '{print $2}')
 
 # macbook pro
 fan1=$(sensors | grep side | awk '{print $4}' | xargs | awk '{print $1}')
 fan2=$(sensors | grep side | awk '{print $4}' | xargs | awk '{print $2}')
-# Emojis and characters for the status bar
-# 💎 💻 💡 🔌 ⚡ 📁 \|
-# echo $uptime_formatted ↑ $linux_version 🐧 $battery_status 🔋 $date_formatted
 
-# lenovo thinkpad
-echo $fan1:$fan2 . $cpu_temp . $battery . $date_formatted .
+# STATUS BAR: Thinkpad
+# echo $fan1:$fan2 . $cpu_temp . $battery . $date_formatted 
 
-# macbook pro
-echo $fan1:$fan2 . $cpu_temp . $date_formatted .
+# STATUS BAR: Macbook pro
+echo $fan1:$fan2 . $cpu . $cpu_temp . $date_formatted 
 
 
