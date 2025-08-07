60ef44b8c9b8b34a2ce10cce302e059801d5f998
jdvfx <ju.dep@zoho.com>
2025-08-06 22:01:10 -0700

    added Onyx config

--- sway/.config/sway/status.sh
+++ sway/.config/sway/status.sh
@@ -1,28 +1,43 @@
 #!/bin/bash
 
-PLATFORM="Thinkpad"
+#PLATFORM="Thinkpad"
+#PLATFORM="MacBookPro"
+PLATFORM="Onyx"
 
 # platform agnostic:
 date_formatted=$(date "+%a.%d.%b.%Y %H:%M")
 # ---------- CPU average usage ---------
 cpu=$(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1"%"}')
 
-if [ "$PLATFORM" == "Thinkpad" ]
-then
-	# thinkpad
+
+case $PLATFORM in
+  Onyx)
+	cpu_temp=$(sensors| grep CPUTIN | cut -d + -f2 | cut -d 'C' -f 1)
+	echo $cpu_temp . $date_formatted
+	;;
+  
+  MacBookPro)
+	fan1=$(sensors | grep side | awk '{print $4}' | xargs | awk '{print $1}')
+	fan2=$(sensors | grep side | awk '{print $4}' | xargs | awk '{print $2}')
+	cpu_temp=$(sensors | grep "Core 0" | awk '{print $3}')
+	
+	echo $fan1:$fan2 . $cpu . $cpu_temp . $date_formatted
+	;;
+
+  Thinkpad)
 	battery=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep percentage | awk '{print $2}')
 
 	cpu_temp=$(sensors| grep CPU | cut -d + -f2)
 	fan1=$(cat /proc/acpi/ibm/fan | grep level: | awk '{print $2}')
 	fan2=$(sensors| grep fan2 | awk '{print $2}')
 
-	echo $fan1:$fan2 . $cpu_temp . $battery . $date_formatted 
-else
-	# macbook pro
-	fan1=$(sensors | grep side | awk '{print $4}' | xargs | awk '{print $1}')
-	fan2=$(sensors | grep side | awk '{print $4}' | xargs | awk '{print $2}')
-	cpu_temp=$(sensors | grep "Core 0" | awk '{print $3}')
-	echo $fan1:$fan2 . $cpu . $cpu_temp . $date_formatted 
+	echo $fan1:$fan2 . $cpu_temp . $battery . $date_formatted
+	;;
+  *)
+	;;
 
-fi
+esac
+
+
+
 
